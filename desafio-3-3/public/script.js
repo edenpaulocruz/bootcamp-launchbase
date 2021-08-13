453ba5b4faf0148c60cf86d9da2d82ee3ce54a57
edenpaulocruz <edenpaulo@gmail.com>
2021-08-13 01:48:25 -0300

    Finalização do desafio-3-3

--- desafio-3-3/public/script.js
+++ desafio-3-3/public/script.js
@@ -1,26 +1,8 @@
-const modal = document.querySelector('.modal');
 const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
-  card.addEventListener("click", function() {
-    const postSlug = card.getAttribute('id');
-    modal.classList.add('modal--active');
-    modal.querySelector("iframe").src = `https://blog.rocketseat.com.br/${postSlug}/`;
+  const programId = card.id;
+  card.addEventListener("click", () => {
+    window.location.href = `/programs/${programId}`;
   })
 }
-
-document.querySelector('.modal__close-icon').
-  addEventListener("click", function() {
-    modal.classList.remove('modal--active');
-    modal.classList.remove('modal--max');
-  });
-
-document.querySelector('.modal__max-icon').
-  addEventListener("click", function() {
-    modal.classList.add('modal--max');
-  });
-
-document.querySelector('.modal__min-icon').
-  addEventListener("click", function() {
-    modal.classList.remove('modal--max');
-  });
--- desafio-3-3/server.js
+++ desafio-3-3/server.js
@@ -8,7 +8,9 @@ const programs = require("./data");
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-  express:server
+  express:server,
+  autoescape: false,
+  noCache: true
 });
 
 server.listen(5000, function() {
@@ -22,6 +24,20 @@ server.get("/", function(req, res) {
 server.get("/programs", function(req, res) {
   return res.render("programs", {items: programs});
 });
+
+server.get("/programs/:programId", function(req, res) {
+  const id = req.params.programId;
+
+  const program = programs.find(program => {
+    return program.id === id;
+  });
+
+  if (!program) {
+    return res.render("not-found");
+  }
+
+  return res.render("program", { program });
+})
 
 server.use(express.static("public"));
 
