f04a14b1622cbf253dea9d6ddaad3da7dc073c59
edenpaulocruz <edenpaulo@gmail.com>
2021-08-12 22:21:45 -0300

    WIP desafio-3-3 - Atualizando os dados, reestilizando os cards e removendo o modal

--- desafio-3-3/data.js
+++ desafio-3-3/data.js
@@ -1,26 +1,23 @@
 module.exports = [
   {
     id: 'discover',
-    logo_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FWuUqKZFhRXqgXspGhcB4&w=64&q=100',
     call: 'Comece a programar do zero',
     image_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2F1dQq4MlT9mpT00oBRCZJ&w=640&q=75',
     title: 'Programa gratuito para iniciantes',
     description: 'Na plataforma você terá acesso a mais de 500 aulas gratuitas para você construir os alicerces necessários para decolar em programação.',
   },
   {
-    id: 'discover',
-    logo_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FWuUqKZFhRXqgXspGhcB4&w=64&q=100',
-    call: 'Comece a programar do zero',
-    image_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2F1dQq4MlT9mpT00oBRCZJ&w=640&q=75',
-    title: 'Programa gratuito para iniciantes',
-    description: 'Na plataforma você terá acesso a mais de 500 aulas gratuitas para você construir os alicerces necessários para decolar em programação.',
+    id: 'ignite',
+    call: 'Domine uma tecnologia específica',
+    image_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FfGBvP9JCQ2awW0INAkD1&w=750&q=75',
+    title: 'Programa para acelerar sua carreira como Dev',
+    description: 'Criado para te preparar para o mercado, desenvolvendo skills técnicas e comportamentais. Evolua e certifique-se em Elixir, Flutter, Node.js, ReactJS ou React Native.',
   },
   {
-    id: 'discover',
-    logo_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FWuUqKZFhRXqgXspGhcB4&w=64&q=100',
-    call: 'Comece a programar do zero',
-    image_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2F1dQq4MlT9mpT00oBRCZJ&w=640&q=75',
-    title: 'Programa gratuito para iniciantes',
-    description: 'Na plataforma você terá acesso a mais de 500 aulas gratuitas para você construir os alicerces necessários para decolar em programação.',
+    id: 'expertsclub',
+    call: 'Aprendizado sob demanda',
+    image_url: 'https://rocketseat.com.br/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FJs7MlXXTyKpxpwZKIDgw&w=1080&q=75',
+    title: 'Clube de aprendizado contínuo para devs',
+    description: 'O Experts Club é um espaço para você se conectar com todo o conhecimento acumulado do mercado através de aulas avançadas e atualizadas semanalmente.',
   }
 ]
